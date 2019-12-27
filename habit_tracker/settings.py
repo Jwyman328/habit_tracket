3e102a6208825bda5fc6191a4dd9f4edfd6388ff
JosephWyman328 <Jwyman@student.framingham.edu>
2019-12-27 11:11:55 -0500

    setting files db fix

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     #'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
-    }
+    #}
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
          'NAME': 'test',                      # Or path to database file if using sqlite3.
