016ce084863bb0250e7730e7e40b1fbe1fb5026a
JosephWyman328 <Jwyman@student.framingham.edu>
2019-12-27 11:05:55 -0500

    deleted all migrations and made initial

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'habit_tracker.wsgi.application'
 DATABASES = {
     #'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+        #'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
+    }
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
          'NAME': 'test',                      # Or path to database file if using sqlite3.
