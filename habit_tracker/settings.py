3cc32909bc64b4e266077d99a7e6d1dc6372c1c0
JosephWyman328 <Jwyman@student.framingham.edu>
2019-12-28 16:34:14 -0500

    setting up database for localProject

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -157,5 +157,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #import django_heroku
 #django_heroku.settings(locals())
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
